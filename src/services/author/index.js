46d7a4887855a44eed1b4fc6cb4dc4f2481e54e9
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-07-13 20:15:30 +0100

    adds GET & POST routes for authors

--- src/services/author/index.js
+++ src/services/author/index.js
@@ -1,11 +1,15 @@
 import { Router } from 'express'
+import db from '../../utils/db/index.js'
 
 const router = Router()
 
 router.route('/')
     .get( async (req, res, next) => {
         try {
-            
+            const query = 'SELECT * FROM authors ORDER BY created_at DESC'
+            const data = await db.query(query)
+
+            res.send(data.rows)
         } catch (error) {
             console.log(error)
             next(error)
@@ -13,7 +17,11 @@ router.route('/')
     })
     .post( async (req, res, next) => {
         try {
-            
+            const { name, surname, avatar } = req.body
+            const query = `INSERT INTO authors ( name, surname, avatar ) VALUES ( '${name}', '${surname}', '${avatar}' ) RETURNING *`
+            const data = await db.query(query)
+
+            res.send(data.rows[0])
         } catch (error) {
             console.log(error)
             next(error)
